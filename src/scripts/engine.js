c7811a12dd9171b75f2090456dc33c2fc481ac4e
renata-aline <renatalinec@gmail.com>
2024-10-31 14:48:32 -0300

    adicionado controle de volume

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,7 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input")
 
+let mapedkeys = [];
 let audio = new Audio("src/tunes/a.wav");
 
 const playTune = (key) =>{
@@ -14,11 +16,19 @@ const playTune = (key) =>{
 };
 
 pianoKeys.forEach((item) => {
-    // console.log(item.dataset.key);
-    item.addEventListener("click", () => playTune(item.dataset.key) )
+    item.addEventListener("click", () => playTune(item.dataset.key))
+    mapedkeys.push(item.dataset.key)
 });
 
 document.addEventListener("keydown", (e) => {
-    playTune(e.key)
+    if(mapedkeys.includes(e.key)){
+        playTune(e.key)
+    }
 });
+
+const handleVolume = (e)=> {
+    audio.volume = e.target.value
+}
+
+volumeSlider.addEventListener("input", handleVolume)
 
