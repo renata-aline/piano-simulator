279edb44433e4cfffd9634be76c8a12a3ce1d574
renata-aline <renatalinec@gmail.com>
2024-10-31 14:18:05 -0300

    adicionado eventos de keydown e adicionado classes dinamicamente

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -3,12 +3,22 @@ const pianoKeys = document.querySelectorAll(".piano-keys .key");
 let audio = new Audio("src/tunes/a.wav");
 
 const playTune = (key) =>{
-    console.log(key)
     audio.src = `src/tunes/${key}.wav`;
     audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`)
+    clickedKey.classList.add("active")
+    setTimeout(()=>{
+        clickedKey.classList.remove("active")
+    },150)
 };
 
 pianoKeys.forEach((item) => {
-    console.log(item.dataset.key);
+    // console.log(item.dataset.key);
     item.addEventListener("click", () => playTune(item.dataset.key) )
 });
+
+document.addEventListener("keydown", (e) => {
+    playTune(e.key)
+});
+
